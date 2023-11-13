8a6ac1a961ac82905a71bf878ad3ab8651a09b4d
George Vasiliou <ge.vasiliou@gmail.com>
2023-11-13 02:32:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
+//echo config('site_url');
+print_r($_GET);
+echo '<br>';
+echo 'Hello from Thingit Mr ' . htmlspecialchars($_GET["name"]) . '!';
+echo '<br>';
+echo '<br>';
+
+foreach($_GET as $key => $value)
+{
+//   echo 'Key = ' . $key . '<br />';
+//   echo 'Value= ' . $value;
+   echo $key.'='.$value.'<br />';
+}
+
+
+/*
     print_r($_POST);
 
     //Or:
-    foreach ($_POST as $key => $value)
+//    foreach ($_POST as $key => $value)
+    foreach ($_GET as $key => $value)
+
         echo $key.'='.$value.'<br />';
+*/
 ?>
-Hello from ThingIt
