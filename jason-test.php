5a37c02887e46ce6480071be425c36bac91cec91
gv <ge.vasiliou@gmail.com>
2023-11-13 03:30:52 +0200

    update

---
+++ jason-test.php
@@ -0,0 +1,30 @@
+<?php
+$json = file_get_contents('php://input');
+echo $json;
+$data = json_decode($json,true);
+echo $data;
+print_r($data);
+
+/* Testing
+curl --header "Content-Type:application/json" --header "Accept: application/json" --request POST --data '{"DT":"08/11/2023 16:35:48","VR":"20906,20761,20739"}' http://127.0.0.1/jason-test.php
+Response:
+ArrayArray
+(
+    [DT] => 08/11/2023 16:35:48
+    [VR] => 20906,20761,20739
+)
+hello
+
+*/
+
+$url = "https://dweet.io/get/latest/dweet/for/gvt";
+$newCurl = curl_init();
+curl_setopt($newCurl, CURLOPT_URL, $url);
+curl_setopt($newCurl, CURLOPT_RETURNTRANSFER, true);
+$data2 = curl_exec($newCurl);
+echo $data2;
+//$data3 = json_decode($output,true);
+$data3 =  json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $data2), true ); 
+echo $data3;
+?>
+hello
